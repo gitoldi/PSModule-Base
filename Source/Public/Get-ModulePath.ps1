e183ca2c524c17a6f9b9dbbf0cc156f9f27136f6
Marcel Rijsbergen <marcel.rijsbergen@centric.eu>
2020-02-26 16:08:10 +0100

    Update Get-ModulePath.ps1

--- Source/Public/Get-ModulePath.ps1
+++ Source/Public/Get-ModulePath.ps1
@@ -1,14 +1,14 @@
 #
 #**********************************************************************
-#   Function: Get-ModulePath - Returns all paths that can contain modules.
+#   Function: Show-ModulePath - Returns all paths that can contain modules.
 #
 #Write-Host "Define function: Get-ModulePath"
-function Get-ModulePath {
+function Show-ModulePath {
     # Get current (sub)routine name.
     #
     #$tmp = $MyInvocation.MyCommand
     $cnt = 1
-    foreach ( $mpad in ($env:PSModulePath).Split( ";" )) {
+    foreach ( $mpad in ( $env:PSModulePath ).Split( ";" )) {
         Write-Host 'Path' $cnt '-' $mpad
         $cnt++
     }
