b0dfb61b8433146cd82afffca7e55a57d333e2dd
Marcel Rijsbergen <Marcel.Rijsbergen@centric.eu>
2021-02-09 16:09:46 +0100

    Added 'Version' for issue #11 and issue #12

--- Source/Public/Show-ModulePath.ps1
+++ Source/Public/Show-ModulePath.ps1
@@ -3,13 +3,33 @@
 #   Function: Show-ModulePath - Returns all paths that can contain modules.
 #
 #Write-Host "Define function: Get-ModulePath"
-function Show-ModulePath {
+
+function Show-ModulePath {    
     # Get current (sub)routine name.
     #
     #$tmp = $MyInvocation.MyCommand
-    $cnt = 1
+    #region 'Initialization.'
+    [ CmdletBinding( )]
+    Param(
+        [ Parameter( ParameterSetName = 'Version' )]
+        [ Switch ] $Version
+    )
+    #endregion 'Initialization.'
+
+    #region 'First get name and return version if requested.'
+    $ScriptName = [ io.path ]::GetFileNameWithoutExtension( $MyInvocation.MyCommand.Name )
+    [ Version ] $ScriptVersion = '1.0.0'
+    if ( $Version ) {
+        Write-Verbose "$ScriptName version : $ScriptVersion"
+        Return $ScriptVersion
+    }
+    #endregion 'First get name and return version if requested.'
+
+    Write-Output -InputObject "$(Get-TimeStamp) $($ScriptName) Current search path for modules:"
+    $Cnt = 0
     foreach ( $mypath in ( $env:PSModulePath ).Split( ";" )) {
-        Write-Host 'Path' $cnt '-' $mypath
-        $cnt++
+        $Cnt++
+        $CntStr = $Cnt.ToString().PadLeft(2)
+        Write-Output -InputObject "`tPath $($CntStr) - $($mypath)"
     }
 }
